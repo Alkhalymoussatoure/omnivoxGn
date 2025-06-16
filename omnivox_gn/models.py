17ef4800c46128c140411901a4a444ab1a7ba52f
Ibrahima-sebe-kourouma <kibse03@gmail.com>
2025-06-16 18:03:26 -0400

    ajout de la classe Etudiant dans models

--- omnivox_gn/models.py
+++ omnivox_gn/models.py
@@ -11,3 +11,16 @@ class Etablissement(models.Model):
 
     def __str__(self):
         return self.nom
+    
+    
+    
+class Etudiant(models.Model):
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+    email = models.EmailField()
+    telephone = models.CharField(max_length=15)
+    date_naissance = models.DateField()
+    etablissement = models.ForeignKey(Etablissement, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.prenom} {self.nom}"
